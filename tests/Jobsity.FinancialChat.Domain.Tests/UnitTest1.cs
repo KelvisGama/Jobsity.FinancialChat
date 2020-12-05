9b65e725d2fbae2885179d005d0c16e7c243af57
Kelvis da Gama <kelvis.gama@laborit.com.br>
2020-12-05 11:03:28 -0300

    add tests projects

---
+++ tests/Jobsity.FinancialChat.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Jobsity.FinancialChat.Domain.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
