5b600d3dd4ed57b7e0382115a2a73018e1b999eb
Kelvis da Gama <kelvis.gama@laborit.com.br>
2020-12-06 16:41:35 -0300

    add dependecy injection extension to persistence project

---
+++ src/3-Infrastructure/Jobsity.FinancialChat.Persistence/DependencyInjection.cs
@@ -0,0 +1,35 @@
+﻿using Jobsity.FinancialChat.Persistence.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jobsity.FinancialChat.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            if (configuration.GetValue<bool>("UseInMemoryDatabase"))
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseInMemoryDatabase("FinancialChatDb"));
+            }
+            else
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(
+                        configuration.GetConnectionString("DefaultConnection")));
+            }
+                       
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
