c96dd6f5f445e56a3a3f21fa746e7d1f341c596d
Kelvis da Gama <kelvis.gama@laborit.com.br>
2020-12-06 18:57:49 -0300

    add DI inversion of control

---
+++ src/3-Infrastructure/Jobsity.FinancialChat.IoC/NativeInjectorBootStrapper.cs
@@ -0,0 +1,16 @@
+﻿using Jobsity.FinancialChat.Application;
+using Jobsity.FinancialChat.Persistence;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Jobsity.FinancialChat.IoC
+{
+    public static class NativeInjectorBootStrapper
+    {
+        public static void RegisterServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddApplication();
+            services.AddPersistence(configuration);
+        }
+    }
+}
