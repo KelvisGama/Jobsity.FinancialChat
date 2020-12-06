d6294425ed75d26cfbe83ef940ba2c9109d869df
Kelvis da Gama <kelvis.gama@laborit.com.br>
2020-12-06 15:04:25 -0300

    add dependecy injection application extension

---
+++ src/2-Core/Jobsity.FinancialChat.Application/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using FluentValidation;
+using Jobsity.FinancialChat.Application.Common.Behaviours;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Jobsity.FinancialChat.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());            
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+
+            return services;
+        }
+    }
+}
