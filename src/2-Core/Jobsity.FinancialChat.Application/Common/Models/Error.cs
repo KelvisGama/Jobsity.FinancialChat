42a18c0aa0e334e54578b2cfca239b3319f4d442
Kelvis da Gama <kelvis.gama@laborit.com.br>
2020-12-06 12:06:53 -0300

    add mediatR validation behavior and custom validation exception

---
+++ src/2-Core/Jobsity.FinancialChat.Application/Common/Models/Error.cs
@@ -0,0 +1,9 @@
+﻿namespace Jobsity.FinancialChat.Application.Common.Models
+{
+    public class Error
+    {
+        public long ErrorCode { get; set; }
+        public string PropertyName { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
