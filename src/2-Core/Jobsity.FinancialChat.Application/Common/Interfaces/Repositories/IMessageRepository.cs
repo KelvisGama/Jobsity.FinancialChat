36d027eff1ee8280ca1789a02fa155db1b0b84a5
Kelvis da Gama <kelvis.gama@laborit.com.br>
2020-12-06 12:26:23 -0300

    add message interface repository

---
+++ src/2-Core/Jobsity.FinancialChat.Application/Common/Interfaces/Repositories/IMessageRepository.cs
@@ -0,0 +1,11 @@
+﻿using Jobsity.FinancialChat.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Jobsity.FinancialChat.Application.Common.Interfaces.Repositories
+{
+    public interface IMessageRepository : IBaseRepository<Message>
+    {
+        Task<IEnumerable<Message>> GetLastFiftyMessages();
+    }
+}
