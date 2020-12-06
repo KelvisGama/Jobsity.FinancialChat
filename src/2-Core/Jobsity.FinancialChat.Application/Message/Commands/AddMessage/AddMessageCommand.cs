73e2806a4211946a8f2b838c2aa9884b9c9d6396
Kelvis da Gama <kelvis.gama@laborit.com.br>
2020-12-06 14:00:12 -0300

    add add message command class

---
+++ src/2-Core/Jobsity.FinancialChat.Application/Message/Commands/AddMessage/AddMessageCommand.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jobsity.FinancialChat.Application.Message.Commands.AddMessage
+{
+    public class AddMessageCommand : IRequest
+    {
+        public string UserName { get; set; }
+        public string Body { get; set; }
+        public DateTime When { get; set; }
+
+        public AddMessageCommand(string userName, string body)
+        {
+            UserName = userName;
+            Body = body;
+            When = DateTime.Now;
+        }
+    }
+}
