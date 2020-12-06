e3f015a666578c431233f9d953e06a1764afbdd7
Kelvis da Gama <kelvis.gama@laborit.com.br>
2020-12-06 14:58:07 -0300

    add get last fifty messages query

--- src/2-Core/Jobsity.FinancialChat.Application/Messages/Commands/AddMessage/AddMessageCommandHandler.cs
+++ src/2-Core/Jobsity.FinancialChat.Application/Messages/Commands/AddMessage/AddMessageCommandHandler.cs
@@ -2,9 +2,6 @@
 using Jobsity.FinancialChat.Application.Common.Interfaces.Repositories;
 using Jobsity.FinancialChat.Domain.Entities;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
---
+++ src/2-Core/Jobsity.FinancialChat.Application/Messages/Queries/GetLastFiftyMessages/MessageDto.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Jobsity.FinancialChat.Application.Common.Interfaces;
+using Jobsity.FinancialChat.Domain.Entities;
+using System;
+
+namespace Jobsity.FinancialChat.Application.Messages.Queries.GetLastFiftyMessages
+{
+    public class MessageDto : IMapFrom<Message>
+    {
+        public string UserName { get; set; }
+        public string Body { get; set; }
+        public DateTime When { get; set; }
+
+        public void Mapping(Profile profile)
+            => profile.CreateMap<Message, MessageDto>();
+    }
+}
