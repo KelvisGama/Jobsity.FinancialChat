d972d0f7214dd63facf28f66e4906b6ac481e21e
Kelvis da Gama <kelvis.gama@laborit.com.br>
2020-12-06 22:05:20 -0300

    fix default users seed

--- src/3-Infrastructure/Jobsity.FinancialChat.Persistence/Context/ApplicationDbContextSeed.cs
+++ src/3-Infrastructure/Jobsity.FinancialChat.Persistence/Context/ApplicationDbContextSeed.cs
@@ -8,27 +8,27 @@ namespace Jobsity.FinancialChat.Persistence.Context
     public static class ApplicationDbContextSeed
     {
         public static async Task SeedDefaultUserAsync(UserManager<IdentityUser> userManager)
-        {            
-            var defaultUsers = new[] { 
-                new IdentityUser { UserName = "Benjamin Graham", Email = "graham@jobsity.com" },
-                new IdentityUser { UserName = "John Templeton", Email = "templeton@jobsity.com" },
-                new IdentityUser { UserName = "John Neff", Email = "neff@jobsity.com" },
-                new IdentityUser { UserName = "Jesse Livermore", Email = "livermore@jobsity.com" },
-                new IdentityUser { UserName = "Peter Lynch", Email = "lynch@jobsity.com" },
-                new IdentityUser { UserName = "George Soros", Email = "soros@jobsity.com" },
-                new IdentityUser { UserName = "Warren Buffett", Email = "buffett@jobsity.com" },
-                new IdentityUser { UserName = "John Bogle", Email = "bogle@jobsity.com" },
-                new IdentityUser { UserName = "Carl Icahn", Email = "icahn@jobsity.com" },
-                new IdentityUser { UserName = "William H. Gross", Email = "gross@jobsity.com" },
+        {
+            var defaultUsers = new[] {
+                new IdentityUser { UserName = "benjamin.graham@jobsity.com", Email = "benjamin.graham@jobsity.com" },
+                new IdentityUser { UserName = "john.templeton@jobsity.com", Email = "john.templeton@jobsity.com" },
+                new IdentityUser { UserName = "john.neff@jobsity.com", Email = "john.neff@jobsity.com" },
+                new IdentityUser { UserName = "jesse.livermore@jobsity.com", Email = "jesse.livermore@jobsity.com" },
+                new IdentityUser { UserName = "peter.lynch@jobsity.com", Email = "peter.lynch@jobsity.com" },
+                new IdentityUser { UserName = "george.soros@jobsity.com", Email = "george.soros@jobsity.com" },
+                new IdentityUser { UserName = "warren.buffett@jobsity.com", Email = "warren.buffett@jobsity.com" },
+                new IdentityUser { UserName = "john.bogle@jobsity.com", Email = "john.bogle@jobsity.com" },
+                new IdentityUser { UserName = "carl.icahn@jobsity.com", Email = "carl.icahn@jobsity.com" },
+                new IdentityUser { UserName = "william.gross@jobsity.com", Email = "william.gross@jobsity.com" },
             };
 
             foreach (var defaultUser in defaultUsers)
             {
-                if (userManager.Users.All(u => u.UserName.Equals(defaultUser.UserName, StringComparison.InvariantCultureIgnoreCase)))
+                if (userManager.Users.All(u => u.UserName != defaultUser.UserName))
                 {
-                    await userManager.CreateAsync(defaultUser, "1234");
+                    await userManager.CreateAsync(defaultUser, "Jobity@123");
                 }
-            }            
+            }
         }
     }
 }
